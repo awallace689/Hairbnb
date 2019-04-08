566c4d1fcf8c077da4a8f1d92dacb22429ec09a1
Miller Bath <millerzbath@gmail.com>
2019-04-07 22:54:41 -0500

    Added telephone, company, Notes, and Past Appointments. Rough work for Prototype.

--- profile_for_user/lib/main.dart
+++ profile_for_user/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() {
-  debugPaintSizeEnabled=true;
+  //debugPaintSizeEnabled=true;
   runApp(MyApp());
 }
 
@@ -28,14 +28,14 @@ class _MyHomePageState extends State<MyHomePage> {
         body: new Stack(
           children: <Widget>[
             ClipPath(
-              child: Container(color: Colors.blue[800].withOpacity(0.8)),
+              child: Container(color: Colors.blue[600]), //TODO: implement random color picker for the user profile
               clipper: getClipper(),
             ),
             Positioned(
                 width: MediaQuery.of(context).size.width-10,
                 top: MediaQuery.of(context).size.height / 20,
                 child: Row(
-                  children: <Widget>[
+                  children: [
                     SizedBox(width: 15),
                     Container( //profile img and box containing it
                         width: 150.0,
@@ -52,25 +52,94 @@ class _MyHomePageState extends State<MyHomePage> {
                               BoxShadow(blurRadius: 7.0, color: Colors.black)
                             ])),
                     SizedBox(width:15),
-                    Text( //profile name
-                      'Tom Cruise',
-                      style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Montserrat'),
+                    Column(
+                      children: [
+                        Text( //profile name
+                          'Tom Cruise',
+                          style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat'),
+                        ),
+                        SizedBox(height:5),
+                        Text(
+                            'Austin Powers Talent Agency',
+                            style: TextStyle(
+                              fontSize: 14,
+                              //fontStyle: FontStyle.italic,
+                              color: Colors.grey[900],
+
+                            ),
+                        ),
+                      ], //column children
                     ),
-                  ],
+                  ], //row children
                 )),
-//            Positioned(
-//              width:500,
-//              top:MediaQuery.of(context).size.height/1.8,
-//              child: Row(
-//                children: <Widget>[
-//                  Text('Testing'),
-//                  Text('Testing again'),
-//                ],
-//              ),
-//            )
+            Positioned(
+              width: MediaQuery.of(context).size.width-10,
+              top:MediaQuery.of(context).size.height/3.1,
+              left:15,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://yakimaymca.org/wp-content/uploads/2017/08/Telephone-icon-orange-300x300.png'),
+                            ),
+                          )),
+                      SizedBox(width:12),
+                      Text(
+                          'Telephone: ',
+                          style: TextStyle(
+                            fontSize: 14,
+                          )
+                      ),
+                      Text(
+                          '(913) 253-2392',
+                          style: TextStyle(
+                            fontSize: 14,
+
+                          )
+                      ),
+
+                      //SizedBox(height: 25),
+                    ], //children for row
+                  ),
+                  SizedBox(height:20),
+                  Row(
+                    children: [ //TODO: Why is there a difference between children <widget> ? seems pretty useless to use the widget one cause it just gives me errors
+                      Text(
+                          'Notes: ',
+                          style: TextStyle(
+                            fontSize: 14,
+                          )
+                      ),
+                      myBox(),
+                      //SizedBox(height:300),
+                    ],
+                  ),
+                  SizedBox(height:20),
+                  Row(
+                    children: [
+                      Text(
+                          'Past appointments ',
+                          style: TextStyle(
+                            fontSize: 14,
+                          )
+                      ),
+                    ],
+                  ),
+
+                ], //children for post
+              ),
+            )
           ],
         ));
   }
@@ -94,4 +163,28 @@ class getClipper extends CustomClipper<Path> {
     // TODO: implement shouldReclip
     return true;
   }
+}
+
+Widget myBox() {
+  return Container(
+    width:300,
+    //margin: const EdgeInsets.all(30.0),
+    padding: const EdgeInsets.all(5.0),
+    decoration: myBoxDecoration(), //             <--- BoxDecoration here
+    child: Text(
+      "These are where the notes go.",
+      style: TextStyle(fontSize: 14.0),
+    ),
+  );
+}
+
+BoxDecoration myBoxDecoration() {
+  return BoxDecoration(
+    border: Border(
+      bottom: BorderSide( //                   <--- left side
+        color: Colors.black,
+        width: 1.0,
+      ),
+    ),
+  );
 }
