92292e7573659c3de656d6c56b291b561fa4aecd
BoHui Lu <a500l206@ku.edu>
2019-05-01 00:12:26 -0500

    Widget test for admin page.

---
+++ test/AdminPage_widget_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:project4/AdminSide/AdminPage.dart';
+import 'package:image_test_utils/image_test_utils.dart';
+
+
+
+void main() {
+
+
+  Widget makeTestableWidget({Widget child}) {
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets('Checking if Appointment tab works properly', (WidgetTester tester) async {
+
+
+    AdminPage page = AdminPage();
+
+    await tester.pumpWidget(makeTestableWidget(child: page));
+
+    await tester.tap(find.byIcon(Icons.calendar_today));
+    await tester.pump();
+
+    expect(find.text('Appointments'), findsOneWidget);
+
+  });
+
+  testWidgets('Checking if My Shop tab works properly', (WidgetTester tester) async {
+
+    provideMockedNetworkImages( () async {
+
+      AdminPage page = AdminPage();
+
+      await tester.pumpWidget(makeTestableWidget(child: page));
+
+      await tester.tap(find.byIcon(Icons.info));
+      await tester.pump();
+
+      expect(find.text('Amyx Barber Shop North'), findsOneWidget);
+
+
+    });
+  });
+
+
+
+
+}
